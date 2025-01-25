cbf702bd162681cd7ce7ef305df565c1f0e69913
Ashraf Abi Said <ashraf.abisaid@gmail.com>
2025-01-25 21:44:32 +0200

    bubbles!

--- Assets/Scripts/BubbleGO.cs
+++ Assets/Scripts/BubbleGO.cs
@@ -6,10 +6,19 @@ public class BubbleGO : MonoBehaviour
     Rigidbody bubbleRb;
     Vector3 bubbleDirection;
     public float timeBeforePopping = 3;
+    float totalTimeBeforePop;
+    [SerializeField] float weightMultiplier;
+    private float weightCarried;
+    [SerializeField] Material bubbleMaterial;
+    [SerializeField] GameObject particlesGO;
     void Start()
     {
         bubbleRb = GetComponent<Rigidbody>();
         bubbleDirection = Vector3.up;
+        bubbleMaterial = GetComponent<Renderer>().material;
+        Debug.Log("Bubble renderer is " + bubbleMaterial.name);
+        Debug.Log("Bubble input is " + bubbleMaterial.GetColor("_External_Color"));
+        totalTimeBeforePop = timeBeforePopping;
     }
 
     void Update()
@@ -17,10 +26,20 @@ public class BubbleGO : MonoBehaviour
         if (timeBeforePopping > 0)
         {
             FloatBubble();
-            timeBeforePopping -= Time.deltaTime;
+            timeBeforePopping -= (Time.deltaTime + weightMultiplier*weightCarried);
+            Debug.Log("Bubble input before is " + bubbleMaterial.GetColor("_External_Color"));
+            Debug.Log("Bubble input before is " + bubbleMaterial.GetColor("_Internal_Color"));
+            float colorVal = (totalTimeBeforePop - timeBeforePopping)/totalTimeBeforePop;
+            bubbleMaterial.SetColor("_External_Color", Color.Lerp(Color.green, Color.red, colorVal));
+            bubbleMaterial.SetColor("_Internal_Color", Color.Lerp(Color.blue, Color.red, colorVal));
+            Debug.Log("Bubble input after is " + bubbleMaterial.GetColor("_External_Color"));
+            Debug.Log("Bubble input after is " + bubbleMaterial.GetColor("_Internal_Color"));
+
         }
         else
         {
+            particlesGO.SetActive(true);
+            Destroy(particlesGO, 2);
             transform.DetachChildren();
             Destroy(gameObject);
         }
@@ -29,5 +48,13 @@ public class BubbleGO : MonoBehaviour
     public void FloatBubble()
     {
         bubbleRb.linearVelocity = bubbleDirection * bubbleSpeed;
+    }
+    public void SetWeightCarried(float weight)
+    {
+        weightCarried = weight;
+    }
+    public void AddSuddenWeight(float addedWeight)
+    {
+        timeBeforePopping -= addedWeight/10;
     }
 }
