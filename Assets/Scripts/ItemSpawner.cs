d8ab88e4bcbe800b516cbcf985269acfcb4ecae6
Ashraf Abi Said <ashraf.abisaid@gmail.com>
2025-01-26 11:06:22 +0200

    item spawner and level manager

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -3,40 +3,14 @@ using UnityEngine;
 
 public class ItemSpawner : MonoBehaviour
 {
-    [SerializeField] ItemGO lightItem;
-    [SerializeField] ItemGO mediumItem;
-    [SerializeField] ItemGO heavyItem;
     [SerializeField] List<SpawnPosition> spawnPositions;
-    [SerializeField] List<ItemGO> lightItemsList;
-    [SerializeField] List<ItemGO> mediumItemsList;
-    [SerializeField] List<ItemGO> heavyItemsList;
-    [SerializeField] int lightItemCount = 5;
-    [SerializeField] int mediumItemCount = 5;
-    [SerializeField] int heavyItemCount = 5;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void SpawnItem(ItemGO itemToAdd)
     {
-        lightItemsList = new List<ItemGO>();
-        mediumItemsList = new List<ItemGO>();
-        heavyItemsList = new List<ItemGO>();
-
-        SpawnItem(lightItemsList, lightItemCount, lightItem);
-        SpawnItem(mediumItemsList, mediumItemCount, mediumItem);
-        SpawnItem(heavyItemsList, heavyItemCount, heavyItem);
-    }
-
-    private void SpawnItem(List<ItemGO> listOfItems, int numOfItems, ItemGO itemToAdd)
-    {
-        for(int i = 0; i < numOfItems; i++)
-        {
             Debug.Log("Spawning items " + itemToAdd.name);
             SpawnPosition spawnPosition = FindEmptySpot();
-            var itemCreated = Instantiate(itemToAdd, spawnPosition.transform.position, spawnPosition.transform.rotation);
+            Instantiate(itemToAdd, spawnPosition.transform.position, spawnPosition.transform.rotation);
             spawnPosition.itemHeld = itemToAdd;
-            listOfItems.Add(itemCreated);
-
-        }
     }
 
     private SpawnPosition FindEmptySpot()
